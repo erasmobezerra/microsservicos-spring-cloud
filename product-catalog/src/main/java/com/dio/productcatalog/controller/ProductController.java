7c40173ca095609511c09ec06aa7038219533c64
erasmobezerra <erasmo.ads.tech@gmail.com>
2021-12-05 13:31:45 -0300

    update

--- product-catalog/src/main/java/com/dio/productcatalog/controller/ProductController.java
+++ product-catalog/src/main/java/com/dio/productcatalog/controller/ProductController.java
@@ -1,19 +1,15 @@
 package com.dio.productcatalog.controller;
 
 import com.dio.productcatalog.models.Product;
-import com.dio.productcatalog.repository.ProductRepository;
 import com.dio.productcatalog.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/product")
 public class ProductController {
-
-    @Autowired
-    private ProductRepository productRepository;
 
     @Autowired
     private ProductService productService;
@@ -24,7 +20,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/{id}")
-    public Optional<Product> findById(@PathVariable("id") Integer id) {
+    public ResponseEntity<Product> findById(@PathVariable("id") Integer id) {
         return productService.findById(id);
     }
 
--- product-catalog/src/main/java/com/dio/productcatalog/service/ProductService.java
+++ product-catalog/src/main/java/com/dio/productcatalog/service/ProductService.java
@@ -2,8 +2,12 @@ package com.dio.productcatalog.service;
 
 import com.dio.productcatalog.models.Product;
 import com.dio.productcatalog.repository.ProductRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Optional;
 
@@ -13,14 +17,18 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @ResponseStatus(HttpStatus.CREATED)
     public Product save(Product product) {
         return productRepository.save(product);
     }
 
-    public Optional<Product> findById(Integer id) {
-        return productRepository.findById(id);
+    public ResponseEntity<Product> findById(Integer id) {
+        Optional<Product> optional = productRepository.findById(id);
+        return optional.map(product -> ResponseEntity.ok().body(product))
+        .orElseGet(() -> ResponseEntity.notFound().build());
     }
-
+   
+    @ResponseStatus(HttpStatus.OK)
     public Iterable<Product> findByName(String name) {
         return productRepository.findByName(name);
     }
